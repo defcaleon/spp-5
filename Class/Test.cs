308c05a58f3ed24a15d7dd25fa5b73c732a8b90b
Nikita Parmon <nikitka.parmon@mail.ru>
2021-11-26 21:20:43 +0300

    class to test realization

---
+++ Class/Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace TestClasses
+{
+    public interface IService
+    {
+        void BestMethod();
+    }
+
+    public interface IGenericService<T>
+    {
+        void BestMethod();
+    }
+
+    public abstract class AbstractService
+    {
+        public abstract void BestMethod();
+    }
+
+    public class GenericClass<T> : IService
+    {
+        public void BestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ServiceImpl1 : IService
+    {
+        public void BestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ServiceImpl2 : IService
+    {
+        public void BestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AbstractServiceImpl : AbstractService
+    {
+        public override void BestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GenericServiceImpl<T> : IGenericService<T>
+    {
+        public void BestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Class1
+    {
+
+    }
+
+    public class ClassWithDependencies
+    {
+        public IService Service;
+
+        public IGenericService<int> GenericService;
+
+        public ClassWithDependencies(IService service, IGenericService<int> genericService)
+        {
+            Service = service;
+            GenericService = genericService;
+        }
+    }
+}
